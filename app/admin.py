bb688fd640981d05e7427bfb400e4697b104e12f
Darwin Salinas <salinash2000@gmail.com>
2019-10-04 05:01:05 +0200

    migraciones y admin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from .models import ObjetoUtilizado
+from .models import Distrito, EstadoDenuncia, TipoLugar, TipoDelito, Sexo, ColorPiel, Contextura, Municipio, Localidad, ObjetoUtilizado, ObjetoAfectado, PresuntoAutor, Victima, Testigo, Denuncia 
 
 admin.site.unregister(Group)
 
@@ -10,4 +10,61 @@ admin.site.site_title = "SDPN"
 admin.site.index_title = "Bienvenido a SDPN"
 
 
-admin.site.register(ObjetoUtilizado)
+class PresuntoAutorInline(admin.TabularInline):
+    model = PresuntoAutor
+    extra = 1
+
+
+class VictimaInline(admin.TabularInline):
+    model = Victima
+    extra = 1
+
+
+class TestigoInline(admin.TabularInline):
+    model = Testigo
+    extra = 1
+
+
+class ObjetoUtilizadoInline(admin.TabularInline):
+    model = ObjetoUtilizado
+    extra = 1
+
+
+class ObjetoAfectadoInline(admin.TabularInline):
+    model = ObjetoAfectado
+    extra = 1
+
+
+
+
+class DenunciaAdmin(admin.ModelAdmin):
+    model = Denuncia
+    inlines = [
+        VictimaInline,
+        PresuntoAutorInline,
+        TestigoInline,
+        ObjetoUtilizadoInline,
+        ObjetoAfectadoInline,
+    ]
+
+    list_filter = ('fecha_hora', 'estado', 'localidad', 'tipo_delito', 'victima_set__sexo')
+
+
+admin.site.register([
+    Distrito, 
+    EstadoDenuncia, 
+    TipoLugar, 
+    TipoDelito, 
+    Sexo, 
+    ColorPiel, 
+    Contextura, 
+    Municipio, 
+    Localidad, 
+    ObjetoUtilizado, 
+    ObjetoAfectado, 
+    PresuntoAutor, 
+    Victima, 
+    Testigo 
+])
+
+admin.site.register(Denuncia, DenunciaAdmin)
--- app/models.py
+++ app/models.py
@@ -1,45 +1,144 @@
 from django.db import models
 
+
+class Distrito(models.Model):
+    nombre = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.nombre
+
+
+class EstadoDenuncia(models.Model):
+    nombre = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.nombre
+
+
 class TipoLugar(models.Model):
     nombre = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.nombre
 
 
 class TipoDelito(models.Model):
     nombre = models.CharField(max_length=35)
 
+    def __str__(self):
+        return self.nombre
 
-#class Sexo(models.Model):
-#    nombre = models.CharField(max_length=1)
+
+class Sexo(models.Model):
+    nombre = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.nombre
 
 
-#class Municipio(models.Model):
-#    nombre = models.CharField(max_length=100)
+class ColorPiel(models.Model):
+    nombre = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Contextura(models.Model):
+    nombre = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Municipio(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
 
 
 class Localidad(models.Model):
     nombre = models.CharField(max_length=50)
-    municipio = models.CharField(max_length=30)
+    municipio = models.ForeignKey(Municipio, on_delete=models.CASCADE)
     distrito_no = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+
+
+class Denuncia(models.Model):
+    fecha_hora = models.DateTimeField()
+    identificacion = models.CharField(max_length=30)
+    estado = models.ForeignKey(EstadoDenuncia, on_delete=models.CASCADE)
+    direccion = models.CharField(max_length=250)
+    sintesis = models.TextField()
+    agresores_masculinos = models.IntegerField()
+    agresores_femeninos = models.IntegerField()
+    agresores_desconocidos = models.IntegerField()
+    localidad = models.ForeignKey(Localidad, on_delete=models.CASCADE)
+    tipo_delito = models.ForeignKey(TipoDelito, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {} ".format(self.identificacion, self.fecha_hora)
 
 
 class PresuntoAutor(models.Model):
     nombre = models.CharField(max_length=60)
-    descripcion = models.TextField(max)
-    color_piel = models.CharField(max_length=15)
-    estatura_aprox = models.FloatField()
-    cabello = models.TextField(max)
-    contextura = models.CharField(max_length=15)
-    tatuajes = models.TextField(max)
-    cicatrices = models.TextField(max)
-    sexo = models.CharField(max_length=12)
     alias = models.CharField(max_length=60)
+    descripcion = models.TextField()
+    contextura = models.ForeignKey(Contextura, on_delete=models.CASCADE)
+    sexo = models.ForeignKey(Sexo, on_delete=models.CASCADE)
+    color_piel = models.ForeignKey(ColorPiel, on_delete=models.CASCADE)
+    estatura_aprox = models.FloatField()
+    cabello = models.TextField()
+    tatuajes = models.TextField()
+    cicatrices = models.TextField()
+    denuncia = models.ForeignKey(Denuncia, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+
+class Victima(models.Model):
+    nombre = models.CharField(max_length=60)
+    identificacion = models.CharField(max_length=30)
+    sexo = models.ForeignKey(Sexo, on_delete=models.CASCADE)
+    edad = models.IntegerField()
+    denuncia = models.ForeignKey(Denuncia, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+    
+
+
+class Testigo(models.Model):
+    nombre = models.CharField(max_length=60)
+    identificacion = models.CharField(max_length=30)
+    sexo = models.ForeignKey(Sexo, on_delete=models.CASCADE)
+    edad = models.IntegerField()
+    denuncia = models.ForeignKey(Denuncia, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
 
 
 class ObjetoUtilizado(models.Model):
     nombre = models.CharField(max_length=70)
     cantidad = models.IntegerField()
+    denuncia = models.ForeignKey(Denuncia, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
 
 
 class ObjetoAfectado(models.Model):
     nombre = models.CharField(max_length=70)
     cantidad = models.IntegerField()
+    denuncia = models.ForeignKey(Denuncia, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
